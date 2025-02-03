af9d25ffc7fe38a6e92f25db56bcdbf2894359f7
Prajwal-PL <prajwalpl096@gmail.com>
2025-02-03 23:02:29 +0530

    Adding routes and controllers, Installing cors and configuring database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
+
 const port = process.env.PORT || 8000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
